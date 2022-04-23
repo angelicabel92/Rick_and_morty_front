cb1157e7654931e25555aabdb41205be8c14d45d
Angelicabel <angelicabel92@gmail.com>
2022-04-23 21:01:28 +0200

    add app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react'
+
+type Props = {};
+
+const App = (props : Props) => {
+  return <div>
+    <Button>Click Here</Button>
+  </div>;
+};
+
+export default App;
---
+++ src/api/request.tsx
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const BASE_URL :string = "https://rickandmortyapi.com/api/";
+
+export const getCharactersRequest = () => {
+    // axios.get(`${BASE_URL}character`)
+     //   .then(res => res)
+     return null
+}
